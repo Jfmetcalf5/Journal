368c226db5a320560993a34e86f75564a467483d
Jfmetcalf5 <Jfmetcalf5@gmail.com>
2018-01-23 18:04:29 -0700

    Initial Commit

---
+++ Journal(retake#3)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Journal(retake#3)
+//
+//  Created by Jacob Metcalf on 1/23/18.
+//  Copyright © 2018 JfMetcalf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
